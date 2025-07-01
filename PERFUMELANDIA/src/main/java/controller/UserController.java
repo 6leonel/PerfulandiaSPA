f9d4bf9bcc39b60ae97dc88b11b0c53a6c149e9d
6smoke <LEON.PAVEZ@DUOCUC.CL>
2025-07-01 19:15:14 -0400

    ada

--- PERFUMELANDIA/src/main/java/controller/UserController.java
+++ PERFUMELANDIA/src/main/java/controller/UserController.java
@@ -2,41 +2,50 @@ package controller;
 
 import model.User;
 import service.UserService;
+import exception.UserNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public EntityModel<User> getUserById(@PathVariable Long id) {
         User user = userService.getUserById(id);
-        if (user != null) {
-            return ResponseEntity.ok(user);
-        }
-        return ResponseEntity.notFound().build();
+        if (user == null) throw new UserNotFoundException("Usuario no encontrado");
+        EntityModel<User> model = EntityModel.of(user);
+        model.add(linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel());
+        model.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("usuarios"));
+        return model;
     }
 
+    @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
-        User updatedUser = userService.updateUser(id, userDetails);
-        if (updatedUser != null) {
-            return ResponseEntity.ok(updatedUser);
-        }
-        return ResponseEntity.notFound().build();
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return userService.updateUser(id, userDetails);
     }
 
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
     }
 }
